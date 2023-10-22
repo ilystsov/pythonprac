1143dd5ece6dd4ffeb4b4e07558b9f03706169ac
ilya <lystsov.ilya@gmail.com>
2023-10-22 18:36:11 +0300

    add spaces parsing

--- 20231017/2/prog.py
+++ 20231017/2/prog.py
@@ -1,10 +1,13 @@
 from math import *
 funcs = {}
+funcs_with_1_param = set()
 str_number = 0
 while s := input():
     if s[0] == ':':
         func_name = s.split()[0][1:]
         params = s.split()[1:-1]
+        if len(params) == 1:
+            funcs_with_1_param.add(func_name)
         params_str = ','.join(params)
         func_str = f'lambda {params_str}:{s.split()[-1]}'
         funcs[func_name] = eval(func_str)
@@ -14,6 +17,9 @@ while s := input():
         break
     else:
         func_name, *params = s.split()
+        if func_name in funcs_with_1_param:
+            params = []
+            params.append(s[s.find(' '):])
         params = (eval(param) for param in params)
         print(funcs[func_name](*params))   
     str_number += 1 
